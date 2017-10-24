b642ab74df2048e95ea98603019c8dea51a68552
Cameron Earl <developer.cameron@gmail.com>
2017-10-24 14:05:57 -0700

    update users migration

--- db/migrations/20171017111235_users.js
+++ db/migrations/20171017111235_users.js
@@ -3,8 +3,8 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', (table) => {//posts is table name.
    table.increments(); //ID add to every table you make.
    table.string('name').notNullable().defaultTo('');
-   table.string('email').notNullable().defaultTo('');
-   table.string('password').notNullable().defaultTo('');
+   table.string('email').notNullable().unique();
+   table.string('password').notNullable();
    table.timestamps(true, true); //created at and updated at.
  });
 };
